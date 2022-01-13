e41e541549c78fc9908a11218cc552478030185c
Rajarshi Banerjee <rajarshiban13@gmail.com>
2022-01-13 18:54:13 +0530

    lol

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ string=''
 cap = cv2.VideoCapture(0)
 
 hm.to720p(cap)
-var = hm.handTrack(1,min_detection_confidence=0.8)
+var = hm.handTrack(1,min_detection_confidence=0.7)
 while 1:
     _, frame = cap.read()
     
@@ -40,8 +40,9 @@ while 1:
     if landmarks:
         for i in Buttons:
             x,y=i.position
-
+            x1,y1 = backspace.position
             depth = abs(landmarks['index'][3][-1])
+            cv2.putText(img,str(depth),(20,500),cv2.FONT_HERSHEY_COMPLEX,3,(0,0,0),3)
 
             if x< landmarks['index'][3][1] < x+i.GAP and y<  landmarks['index'][3][2] <y+i.GAP and depth>300:
                 
@@ -49,8 +50,18 @@ while 1:
                 cv2.putText(img,i.text,(i.position[0]+5,i.position[1]+40 ),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),1)
             
                 string = string + i.text
-        
-        
+
+            if x1<  landmarks['index'][3][1] <x1+backspace.GAP and  depth>300 and  y1<  landmarks['index'][3][2] <y1+backspace.GAP:
+
+                cv2.rectangle(img,backspace.position,(x1+backspace.GAP,y1+backspace.GAP),(255, 255, 0),cv2.FILLED)
+                cv2.putText(img,i.text,(x1+5,y1+40),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),1)
+                if string!='':
+                    string = string.rstrip(string[-1])
+
+        # d = var.dis_btw_2points(0,12)
+        # print(d)
+
+    cv2.putText(img,string,(100+5,600+40),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),0)
     cv2.imshow('op',img)
 
     if cv2.waitKey(1)==27:
